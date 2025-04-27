b3396a32e3e64b6b5f2f9864271d22676f47d586
joaomarcosribeiretee <jmribeirete@hotmail.com>
2025-04-27 14:17:54 -0300

    Organização inicial: limpeza do frontend e configuração do React Router

---
+++ frontend/src/pages/CadastroAluno.jsx
@@ -0,0 +1,11 @@
+function CadastroAluno() {
+    return (
+      <div>
+        <h1>Cadastro de Aluno</h1>
+        <p>Aqui você poderá cadastrar novos alunos.</p>
+      </div>
+    );
+  }
+  
+  export default CadastroAluno;
+  
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <div>
+        <h1>Bem-vindo ao Sistema ULTRAFIT 🏋️‍♂️</h1>
+        <p>Escolha uma opção no menu.</p>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
